4c1c9acc92574b25976c9f13c06660820bd379bf
Serhii Basok <sergeybsk19@gmail.com>
2025-09-20 18:16:50 +0300

    fix: correct validation for Book serializer

--- library/serializers.py
+++ library/serializers.py
@@ -1,11 +1,13 @@
-from rest_framework import serializers
 from models import Book
+from rest_framework import serializers
+
 
 class BookSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
+
 
 class BookListSerializer(serializers.ModelSerializer):
 
